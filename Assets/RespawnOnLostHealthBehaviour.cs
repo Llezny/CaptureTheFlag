10ccbacd096eddf89e9f80ac6da96c7d15ca5e66
Llezny <lukaszlezny@gmail.com>
2023-04-26 17:40:38 +0200

    Added respawn on hit.

--- Assets/RespawnOnLostHealthBehaviour.cs
+++ Assets/RespawnOnLostHealthBehaviour.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class RespawnOnLostHealthBehaviour : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class RespawnOnLostHealthBehaviour : MonoBehaviour {
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private PlayerHealthBehaviour playerHealthBehaviour;
+
+    private void OnEnable( ) {
+        playerHealthBehaviour.OnHit += Respawn;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+    private void OnDisable( ) {
+        playerHealthBehaviour.OnHit -= Respawn;
+    }
+    
+    private void Respawn( ) {
+        transform.position = respawnPoint.position;
     }
 }
--- Assets/Scripts/PlayerHealthBehaviour.cs
+++ Assets/Scripts/PlayerHealthBehaviour.cs
@@ -6,16 +6,16 @@ using UnityTemplateProjects;
 
 public class PlayerHealthBehaviour : MonoBehaviour {
 
-
+    public Action OnHit;
     public IStat<int> health = new Health( );
 
-    private void Awake( ) {
+    private void Start( ) {
         health.Set( 3 );
     }
 
-
     public void GetHit( ) {
         health.Decrease(  );
+        OnHit?.Invoke(  );
     }
 }
        
