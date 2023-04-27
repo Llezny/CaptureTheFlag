0258ffdf2f433ff9eec63a652c444847acf83d20
Llezny <lukaszlezny@gmail.com>
2023-04-27 19:51:07 +0200

    Project cleanup.

--- Assets/Scripts/Zombie/ZombieWanderingSMB.cs
+++ Assets/Scripts/Zombie/ZombieWanderingSMB.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 
 namespace Zombie {
     public class ZombieWanderingSMB : SceneLinkedSMB<ZombieBehaviour> {
-
-        public override void OnSLStateEnter( Animator animator, AnimatorStateInfo stateInfo, int layerIndex ) {
-            base.OnSLStateEnter( animator, stateInfo, layerIndex );
-            Debug.Log("wandering");
-        }
-
         public override void OnSLStateNoTransitionUpdate( Animator animator, AnimatorStateInfo stateInfo, int layerIndex ) {
             base.OnSLStateNoTransitionUpdate( animator, stateInfo, layerIndex );
             if ( m_MonoBehaviour.FindTarget( out var target ) ) {
